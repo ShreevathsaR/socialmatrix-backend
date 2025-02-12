62c4deab730483881d88946c1666e0621a2e5506
ShreevathsaR <103721643+TunedMajor08@users.noreply.github.com>
2025-02-12 17:03:37 +0530

    cleared credentials

--- config/youtube-config.js
+++ config/youtube-config.js
@@ -7,7 +7,7 @@ console.log('APIKEY', process.env.YOUTUBE_API_KEY)
 
 const youtube = google.youtube({
     version: 'v3',
-    auth: process.env.YOUTUBE_API_KEY || 'AIzaSyDpElfgc5SsG7GbJcIbpLhw4ckKMdDmlzI',
+    auth: process.env.YOUTUBE_API_KEY,
 })
 
 module.exports = { youtube }
