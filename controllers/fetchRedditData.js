957b6593d21d15af9804b70f70939110f42c5280
pavan <pavann22826@gmail.com>
2025-02-17 17:28:38 +0530

    added top comments

--- controllers/fetchRedditData.js
+++ controllers/fetchRedditData.js
@@ -40,6 +40,28 @@ async function getRedditAccessToken() {
     }
 }
 
+
+async function getTopComments(postId, token) {
+    try {
+        const response = await axios.get(`https://oauth.reddit.com/comments/${postId}`, {
+            params: { sort: 'hot' },
+            headers: { Authorization: `Bearer ${token}`, 'User-Agent': `${APP_NAME}/0.1 by ${USERNAME}` }
+        })
+
+        const commentsData = response.data[1].data.children
+        const topComments = commentsData.filter(comment => comment.kind === 't1').slice(0, 3).map(comment => ({
+            body: comment.data.body,
+            upvotes: comment.data.ups
+        }))
+    
+        return topComments
+    } catch (error) {
+        console.error("error fetching top comments: ", error.message)
+        return []
+    }
+}
+
+
 async function fetchRedditData(keyword) {
     const token = await getRedditAccessToken()
 
@@ -47,7 +69,7 @@ async function fetchRedditData(keyword) {
         try {
             const [postResponse, subredditResponse] = await Promise.all([
                 axios.get(`https://oauth.reddit.com/search`, {
-                    params: { q: keyword, limit: 15, sort: 'top', t: 'week' },
+                    params: { q: keyword, limit: 15, sort: 'top', t: 'month' },
                     headers: { Authorization: `Bearer ${token}`, 'User-Agent': `pavan/0.1 by ${USERNAME}` }
                 }),
                 axios.get('https://oauth.reddit.com/subreddits/search', {
@@ -56,15 +78,26 @@ async function fetchRedditData(keyword) {
                 })
             ])
 
-            const posts = postResponse.data.data.children.map(post => ({
-                title: post.data.title,
-                subreddit: post.data.subreddit,
-                created_at: new Date(post.data.created_utc * 1000),
-                upvotes: post.data.ups,
-                downvotes: post.data.downs,
-                comments: post.data.num_comments,
-                url: post.data.url
-            }))
+            const posts = await Promise.all(
+                postResponse.data.data.children.map(async (post, index) => {
+                    let postData = {
+                        title: post.data.title,
+                        subreddit: post.data.subreddit,
+                        createdAt: new Date(post.data.created_utc * 1000),
+                        upvotes: post.data.ups,
+                        downvotes: Math.round((post.data.ups / post.data.upvote_ratio) - post.data.ups),
+                        upvoteRatio: post.data.upvote_ratio * 100,
+                        comments: post.data.num_comments,
+                        totalInteraction: Math.round(post.data.upvotes + post.data.num_comments),
+                        url: post.data.url
+                    }
+                    if (index < 3) {
+                        postData.topComments = await getTopComments(post.data.id, token)
+                    }
+
+                    return postData
+                })
+            )
 
             const subreddits = subredditResponse.data.data.children.map(subreddit => ({
                 name: subreddit.data.display_name,
@@ -73,8 +106,25 @@ async function fetchRedditData(keyword) {
                 subscribers: subreddit.data.subscribers,
                 url: subreddit.data.url
             }))
-    
-            return { posts, subreddits }
+
+            //trending posts over the time
+            const timeRanges = ['hour', 'day', 'week']
+            const requests = timeRanges.map(time => 
+                axios.get('https://oauth.reddit.com/search', {
+                    params: { q: keyword, limit: 3, sort: 'top', t: time },
+                    headers: { Authorization: `Bearer ${token}`, 'User-Agent': `${APP_NAME}/0.1 by ${USERNAME}` }
+                }).then(response => ({
+                    time: `trending posts ${time} ago`,
+                    posts: response.data.data.children.map(post => ({
+                        title: post.data.title,
+                        upvotes: post.data.ups,
+                        url: post.data.url
+                    }))
+                }))
+            )
+            const trendingPosts = await Promise.all(requests)
+
+            return { posts, subreddits, trendingPosts }
         } catch (error) {
             console.error("error fetching posts: ", error.message)
             throw new Error("Failed to fetch Reddit data")
